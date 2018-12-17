8a747892aef5485a1705fbfc45974357c9629344
flotisable <s09930698@gmail.com>
2018-12-17 11:42:21 +0800

    do not sort the items at the beginning

--- Src/Knapsack01.cpp
+++ Src/Knapsack01.cpp
@@ -30,10 +30,11 @@ int Knapsack01::solve()
     mNextDecisions.pop();
 
     if( decision.upperBound < mLowerBound ) continue;
-    if( decision.value && ( evalCapcity() < item.weight ) ) continue;
 
     for( size_t i = mDecisions.size() ; i > decision.level ; --i )
        mDecisions.pop_back();
+
+    if( decision.value && ( evalCapcity() < item.weight ) ) continue;
 
     mDecisions.push_back( decision.value );
 
@@ -56,10 +57,11 @@ int Knapsack01::solve()
  */
 int Knapsack01::init()
 {
-  int profit    = 0;
-  int capacity  = mCapacity;
+  vector<Item>  items     = mItems;
+  int           profit    = 0;
+  int           capacity  = mCapacity;
 
-  sort( mItems.begin(), mItems.end(),
+  sort( items.begin(), items.end(),
         []( const Item &front, const Item &back )
         {
           return ( static_cast<double>( front.profit ) / front.weight > static_cast<double>( back.profit ) / back.weight );
@@ -86,10 +88,17 @@ int Knapsack01::init()
  */
 int Knapsack01::upperbound( double profit, double capacity, size_t level )
 {
-  for( size_t i = level ; i < mItems.size() ; ++i )
-  {
-     Item &item = mItems[i];
+  vector<Item> items{ mItems.begin() + level, mItems.end() };
 
+  sort( items.begin(), items.end(),
+        []( const Item &front, const Item &back )
+        {
+          return ( static_cast<double>( front.profit ) / front.weight > static_cast<double>( back.profit ) / back.weight );
+        }
+      );
+
+  for( const Item &item : items )
+  {
      if( capacity > item.weight )
      {
        profit   += item.profit;
